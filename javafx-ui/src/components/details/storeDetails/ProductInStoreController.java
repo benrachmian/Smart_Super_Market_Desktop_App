b4e5d42bbf490bcecfa7cdc79208ca0cb6c9f576
Ben <benrachmian@gmail.com>
2020-09-04 13:35:36 +0300

    products in store details

---
+++ javafx-ui/src/components/details/storeDetails/ProductInStoreController.java
@@ -0,0 +1,59 @@
+package components.details.storeDetails;
+
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.SimpleFloatProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+public class ProductInStoreController {
+
+    private SimpleIntegerProperty productId;
+    private SimpleStringProperty productName;
+    private SimpleStringProperty wayOfBuying;
+    private SimpleFloatProperty productPrice;
+    private SimpleFloatProperty productTotalBought;
+
+    @FXML private GridPane productGridPane;
+    @FXML private Label productIdLabel;
+    @FXML private Label productNameLabel;
+    @FXML private Label wayOfBuyingLabel;
+    @FXML private Label productPriceLabel;
+    @FXML private Label productTotalBoughtLabel;
+
+//    public ProductInStoreController(int productId, String productName, String wayOfBuying, float productPrice, float productTotalBought) {
+//        this.productId = new SimpleIntegerProperty(productId);
+//        this.productName = new SimpleStringProperty(productName);
+//        this.wayOfBuying = new SimpleStringProperty(wayOfBuying);
+//        this.productPrice = new SimpleFloatProperty(productPrice);
+//        this.productTotalBought = new SimpleFloatProperty(productTotalBought);
+//    }
+
+    public ProductInStoreController()
+    {
+        this.productId = new SimpleIntegerProperty();
+        this.productName = new SimpleStringProperty();
+        this.wayOfBuying = new SimpleStringProperty();
+        this.productPrice = new SimpleFloatProperty();
+        this.productTotalBought = new SimpleFloatProperty();
+    }
+
+    @FXML
+    private void initialize() {
+        productIdLabel.textProperty().bind(productId.asString());
+        productNameLabel.textProperty().bind(productName);
+        wayOfBuyingLabel.textProperty().bind(wayOfBuying);
+        productPriceLabel.textProperty().bind(productPrice.asString());
+        productTotalBoughtLabel.textProperty().bind(productTotalBought.asString());
+    }
+
+    public void initDetails(int productSerialNumber, String productName, String wayOfBuying, float price, float amountSoldInStore) {
+        this.productId.set(productSerialNumber);
+        this.productName.set(productName);
+        this.wayOfBuying.set(wayOfBuying);
+        this.productPrice.set(price);
+        this.productTotalBought.set(amountSoldInStore);
+    }
+}
--- javafx-ui/src/components/details/storeDetails/StoreDetailsController.java
+++ javafx-ui/src/components/details/storeDetails/StoreDetailsController.java
@@ -1,15 +1,27 @@
 package components.details.storeDetails;
 
 import SDMSystem.system.SDMSystem;
+import SDMSystemDTO.product.DTOProduct;
+import SDMSystemDTO.product.DTOProductInStore;
 import SDMSystemDTO.store.DTOStore;
+import components.main.SDMMainControllers;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TabPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class StoreDetailsController {
 
     private SDMSystem sdmSystem;
+    private GridPane productGridPane;
+    private ProductInStoreController productInStoreController;
 
     @FXML private TabPane storeTabPane;
     @FXML private ScrollPane productTabScrollPane;
@@ -17,6 +29,9 @@ public class StoreDetailsController {
     @FXML private Label storeNameAnswerLabel;
     @FXML private Label storePpkAnswerLabel;
     @FXML private Label totalProfitFromDeliveryAnswerLabel;
+    @FXML private FlowPane productsFlowPane;
+
+
 
     public void setSdmSystem(SDMSystem sdmSystem) {
         this.sdmSystem = sdmSystem;
@@ -32,5 +47,46 @@ public class StoreDetailsController {
         storeNameAnswerLabel.setText(store.getStoreName());
         storePpkAnswerLabel.setText(String.format("%.2f",store.getPpk()));
         totalProfitFromDeliveryAnswerLabel.setText(String.format("%.2f",store.getTotalProfitFromDelivery()));
+        productsFlowPane.getChildren().clear();
+        updateProducts(store);
+    }
+
+    private void updateProducts(DTOStore store) {
+        for(DTOProductInStore product : store.getProductsInStore().values()){
+            //GridPane newProductGridPane = getGridPaneWithSameProperties();
+
+            //for(Node node : productGridPane.getChildren()){
+                loadProductInStoreDetails();
+                //productInStoreController = new ProductInStoreController();
+                productInStoreController.initDetails(
+                        product.getProductSerialNumber(),
+                        product.getProductName(),
+                        product.getWayOfBuying().toString(),
+                        product.getPrice(),
+                        product.getAmountSoldInStore());
+
+                productsFlowPane.getChildren().add(productGridPane);
+
+
+//                if(node.getId() != null && node.getId().equals("productNameLabel")){
+//                    ((Label)node).setText("ben");
+//                }
+           // }
+        }
+    }
+
+    private void loadProductInStoreDetails() {
+        FXMLLoader loader;
+        URL mainFXML;
+        loader = new FXMLLoader();
+        mainFXML = getClass().getResource("/components/details/storeDetails/productInStoreDetails.fxml");
+        loader.setLocation(mainFXML);
+        try {
+            productGridPane = loader.load();
+            productInStoreController = loader.getController();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
